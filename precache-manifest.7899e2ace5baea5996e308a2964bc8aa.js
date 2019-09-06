3f93e4254af8a09282050036a8935fee99a78092
jmschindele <jmschindele1s@gmail.com>
2019-09-06 19:52:15 -0400

    Updates

---
+++ precache-manifest.7899e2ace5baea5996e308a2964bc8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13fde240903c86493919feced288d81",
+    "url": "/react_tetris/index.html"
+  },
+  {
+    "revision": "270fcb058a3d1e7e7370",
+    "url": "/react_tetris/static/css/main.03aca764.chunk.css"
+  },
+  {
+    "revision": "908d01c0aa66cc2188b7",
+    "url": "/react_tetris/static/js/2.765438a3.chunk.js"
+  },
+  {
+    "revision": "270fcb058a3d1e7e7370",
+    "url": "/react_tetris/static/js/main.b294f5b0.chunk.js"
+  },
+  {
+    "revision": "f4521e43ce751464d51c",
+    "url": "/react_tetris/static/js/runtime~main.df2643eb.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react_tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react_tetris/static/media/bg.2810fe6b.png"
+  }
+]);
